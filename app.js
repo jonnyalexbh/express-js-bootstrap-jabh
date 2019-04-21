31026265e37314d1a52b82896b70ff1fb42ca53b
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-21 11:40:24 -0500

    install configure and test body-parser

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// app
 const app = express();
 
+// middlewares
+app.use(bodyParser.json());                         // support parsing of application/json type post data
+app.use(bodyParser.urlencoded({ extended: true })); // support parsing of application/x-www-form-urlencoded post data
+
+// routes
 app.get('/', function (req, res, next) {
   res.send({ greeting: "hello world" });
 })
 
-const server = app.listen(3000, function () {
-  console.log(`listening http://localhost:${server.address().port}`);
+app.post('/store-user', function (req, res, next) {
+  console.log(req.body);
+})
+
+const server = app.listen(8000, function () {
+  console.log(`REST API running on http://localhost:${server.address().port}`);
 });
