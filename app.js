36536e9eac76046c6e2c9e2d47862195a0baff78
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-21 10:10:41 -0500

    install nodemon and refactor hello world

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', function (req, res, next) {
+  res.send({ greeting: "hello world" });
+})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const server = app.listen(3000, function () {
+  console.log(`listening http://localhost:${server.address().port}`);
 });
---
+++ readme.md
@@ -0,0 +1,19 @@
+expressjs bootstrap
+=================
+
+``` bash
+# main file
+app.js
+```
+### Install the project dependencies:
+``` bash
+cd express-js-bootstrap/
+npm install
+```
+
+### Different ways of Running project :
+``` bash
+npm run dev
+npm run start
+nodemon
+```
