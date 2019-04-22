1d162801dbb984269eb1f6d25ff04d51cabedb1f
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-21 19:57:19 -0500

    middleware, applies for all routes

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,18 @@ const router = express.Router();
 const olderAgeMiddleware = require('./middlewares/olderAge');
 const isAdminMiddleware = require('./middlewares/isAdmin');
 
+// middleware, applies for all routes
+router.use((req, res, next) => {
+  console.log('Time:', Date.now());
+  next();
+});
+
 router.get('/', (req, res) => {
   res.send({ greeting: 'hello world' });
+});
+
+router.get('/other-test', (req, res) => {
+  res.send('other route');
 });
 
 router.get('/show-age/:age/:admin?', [isAdminMiddleware, olderAgeMiddleware], (req, res) => {
