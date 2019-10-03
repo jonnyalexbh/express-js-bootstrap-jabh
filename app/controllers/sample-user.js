fe616318cc2bf8cddd7a4159b0f637684f4508dc
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-02 19:20:55 -0500

    list and search users

--- app/controllers/sample-user.js
+++ app/controllers/sample-user.js
@@ -1,4 +1,20 @@
 const db = require('../models');
+const logger = require('../logger');
+
+exports.sampleUsers = async (_, res) => {
+  const user = await db.User.findAll();
+  res.status(200).send(user);
+};
+
+exports.sampleShowUser = async (req, res) => {
+  try {
+    const user = await db.User.findByPk(req.params.id);
+    res.status(200).send(user);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send(error);
+  }
+};
 
 exports.sampleCreateUser = async (req, res) => {
   const user = await db.User.create({
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 const { main, sampleMiddleware } = require('./controllers/sample');
-const { sampleCreateUser } = require('./controllers/sample-user');
+const { sampleUsers, sampleShowUser, sampleCreateUser } = require('./controllers/sample-user');
 const isAdminMiddleware = require('./middlewares/isAdmin');
 const olderAgeMiddleware = require('./middlewares/olderAge');
 
 exports.init = (app) => {
   app.get('/', main);
   app.get('/sample-middle/:age/:admin?', [isAdminMiddleware, olderAgeMiddleware], sampleMiddleware);
+  app.get('/sample-users', sampleUsers);
+  app.get('/sample-show-user/:id', sampleShowUser);
   app.post('/sample-create-user', sampleCreateUser);
 };
