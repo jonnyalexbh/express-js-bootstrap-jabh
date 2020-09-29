45586a8855afab871633c269aae3776222ca2a01
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-09-29 12:23:08 -0500

    Revert "validate store country with express validation"
    This reverts commit 714f044142e08701bf81ff98634ed3a6f83e3ba0.

--- app/controllers/countries.js
+++ app/controllers/countries.js
@@ -2,7 +2,3 @@ exports.getCountry = (req, res) => {
   const { countryId } = req.query;
   res.send(`should the country id ${countryId}`);
 };
-
-exports.storeCountry = (req, res) => {
-  res.send(req.body);
-};
